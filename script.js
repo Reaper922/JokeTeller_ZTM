9b826c47354036818ea00ef7154a5aa43f2a6325
Dennis Ammen <dennis.ammen@outlook.com>
2022-05-27 22:02:03 +0200

    implemented joke API

--- script.js
+++ script.js
@@ -17,5 +17,21 @@ function textToSpech(joke) {
     });
 }
 
-textToSpech("This is a joke");
+async function getJoke() {
+    let joke = '';
+    const apiUrl = 'https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit';
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        if (data.setup) {
+            joke = `${data.setup} ... ${data.delivery}`;
+        } else {
+            joke = data.joke;
+        }
+        console.log(joke)
+    } catch (error) {
+        console.log(error);
+    }
+}
 
+getJoke();
